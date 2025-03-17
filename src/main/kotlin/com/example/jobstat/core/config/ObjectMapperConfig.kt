e3d0293cb8aef7b519f9163978530d702cd91c36
lsg <dltmdrb1996@naver.com>
2025-03-17 21:43:40 +0900

    fix : was 서버의 cpu 병목의 원인이 json 직렬화 비용이라 가정하에 objectMapper 옵션 변경

--- src/main/kotlin/com/example/jobstat/core/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/example/jobstat/core/config/ObjectMapperConfig.kt
@@ -1,6 +1,7 @@
 package com.example.jobstat.core.config
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -18,6 +19,13 @@ class ObjectMapperConfig {
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                // 성능 최적화 설정 추가
+                .configure(SerializationFeature.FLUSH_AFTER_WRITE_VALUE, false)
+                .configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true)
+                .configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+                // 불필요한 기능 비활성화
+                .configure(MapperFeature.USE_GETTERS_AS_SETTERS, false)
+                .configure(MapperFeature.AUTO_DETECT_IS_GETTERS, false)
     }
 
     @Bean
